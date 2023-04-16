868c6aa4881517d66a8851f74d65ad74f9a44c23
rafayshahood <rafayshahoodtcs7@gmail.com>
2023-04-16 10:06:10 +0500

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import ImageSlider from "./SlideShow";
-
-import frontDesk from './images/frontDesk.JPG'
-import catering from './images/Chairs.JPG'
-import trustdanceFloor from './images/trust&danceFloor.JPG'
-import stageDecor from './images/stageDecor.JPG'
-
+import ImageSlider from "./Components/SlideShow";
+import Map from './Components/Map'
+import images from './imagesUrl.js';
+import Header from './Components/Header';
+import Footer from './Components/Footer'
+import OpenGraph from './Components/OpenGraph';
+import Services from './Components/Services';
+const { frontDesk, catering, trustdanceFloor, stageDecor, coopEventHallDecor, coopEventLobbyDecor, sweetBar, frontPic, frontPic2, stageDecor2 } = images;
 
 
 
@@ -14,82 +15,51 @@ function App() {
 
   const images = [frontDesk,stageDecor,trustdanceFloor];
 
-  const data = [
+  const sliderImages = [
     {
-      image: frontDesk,
-      caption: "San Francisco"
+      url: coopEventLobbyDecor,
+      caption: `"Welcome to Queen's Palace Marquee - Where Your Dream Wedding Comes to Life"`
     },
     {
-      image: stageDecor,
-      caption: "Scotland"
+      url: coopEventHallDecor,
+      caption: `"Cooperate Event @QueensPalaceMarquee"`
     },
     {
-      image: trustdanceFloor,
-      caption: "Darjeeling"
+      url: trustdanceFloor,
+      caption: `"Walima Event @QueensPalaceMarquee"`
+    },
+    {
+      url: frontPic2,
+      caption: `"A stunning entrance to your dream wedding at Queen's Palace Marquee."`
+    },
+    {
+      url: sweetBar,
+      caption: `"Sweet Bar (optional)"`
+    },
+    {
+      url: stageDecor,
+      caption: `"Transform your wedding venue into a fairy tale setting with our exquisite stage decorations."`
+    },
+    {
+      url: stageDecor2,
+      caption: `"Set the stage for your special day with our expert team of decorators and beautiful decorations."`
+    },
+    {
+      url: catering,
+      caption: `"Our customizable catering options ensure that every guest's taste buds are satisfied!"`
     },
   ];
 
-  const slides = [
-    { url: frontDesk, title: "beach" },
-    { url: catering, title: "boat" },
-    { url: trustdanceFloor, title: "forest" },
-    { url: catering, title: "city" },
-    { url: trustdanceFloor, title: "italy" },
-  ];
-  const containerStyles = {
-    width: "80%",
-    height: "500px",
-    margin: "0 auto",
-  };
 
   return (
+    
     <div className="App">
-    <div className="landing-page">
-      <div className="header">
-        <h1>Queen's Palace Marquee</h1>
-        <p>Make your special day unforgettable</p>
-      </div>
-      <div style={containerStyles}>
-        <ImageSlider slides={slides} />
-      </div>
-      <div className="services-section">
-        <h2>Our Services</h2>
-        <div className="services-container">
-          <div className="service-card">
-            <img src={catering} alt="Catering" />
-            <h3>Catering</h3>
-            <p>We offer a variety of customizable catering options to fit your needs and preferences.</p>
-          </div>
-          <div className="service-card">
-            <img src={stageDecor}alt="Decorations" />
-            <h3>Decorations</h3>
-            <p>We have an expert team of decorators who will create a beautiful and elegant ambiance for your wedding.</p>
-          </div>
-          <div className="service-card">
-            <img src={trustdanceFloor} alt="Photography" />
-            <h3>Photography</h3>
-            <p>Our professional photographers will capture your special moments and create timeless memories.</p>
-          </div>
-        </div>
-      </div>
-      <div className="contact-section">
-        <h2>Contact Us</h2>
-        <form>
-          <div className="form-group">
-            <input type="text" placeholder="Name" />
-          </div>
-          <div className="form-group">
-            <input type="email" placeholder="Email" />
-          </div>
-          <div className="form-group">
-            <textarea placeholder="Message"></textarea>
-          </div>
-          <div className="form-group">
-            <button>Submit</button>
-          </div>
-        </form>
-      </div>
-    </div>
+      <OpenGraph img={frontDesk}/>
+      <Header/>
+      <ImageSlider slides={sliderImages} />
+      <Services/>
+      <Map/>
+      <Footer/>
     </div>
   );
 }
